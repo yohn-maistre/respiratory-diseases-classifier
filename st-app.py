f5f734c353f1598ec5533dd3f4e1c95e3ff89852
yohn-maistre <josejr2498@gmail.com>
2023-03-13 00:32:44 +0900

    ui

--- st-app.py
+++ st-app.py
@@ -16,17 +16,13 @@ import seaborn as sns
 #import pdb
 
 st.title('Prediksi Penyakit Saluran Pernapasan')
-st.caption('*Made with ❤️ by Yose Marthin Giyay*')
-st.markdown('**Konteks Model AI dan Database:**')
-st.caption('Model yang digunakan merupakan Convolutional Neural Network (CNN) yang dilatih menggunakan **TensorFlow 2.11.0**. Model ini dilatih dengan data dari **Respiratory Sound Database** yang dikemas 2 tim peneliti di Portugal dan Yunani atas nama *International Conference on Biomedical Health Informatics* (ICHBI)')
-st.caption('Di sini _library_ **Librosa** digunakan untuk ekstraksi MFCCs dari fail audio. MFCC, atau Mel-Frequency Cepstral Coefficients, merupakan format representasi audio. Dengan proses matematis ini, fitur-fitur penting di frekuensi alami telinga manusia dapat diekstraksi dari fail audio dan dijadikan *input* ke model CNN untuk diprediksi.')
-st.caption('Jurnal ilmiah menyangkut database yang digunakan dapat dilihat di sini: https://link.springer.com/chapter/10.1007/978-981-10-7419-6_6')
+st.caption('*Made with ❤️  by Yose Marthin Giyay*')
+
 st.subheader('**Terdapat 6 kategori diagnosis:**')
 st.markdown('*- Sehat*   \n*- Bronkiektasis*   \n*- Bronkiolitis*   \n*- Penyakit Paru Obstruktif Kronis (PPOK)*   \n*- Pneumonia*   \n*- Infeksi Saluran Pernapasan Atas*')
-
 st.subheader('Unggah fail audio dan mulai prediksi')
-st.caption('*Dalam pengembangan: rekam langsung*. **Untuk sekarang, unggah fail audio 20 detik**.')
-st.caption('Untuk rekam audio dengan format WAVE (.wav) di HP (jika tidak didukung), gunakan aplikasi seperti AudioLab; *record* dengan pilihan settings *Audio format: WAVE*, dan klik *Browse files* di laman ini untuk unggah.')
+st.caption('*Dalam pengembangan: rekam langsung*. **Untuk sekarang, silakan unggah fail audio .wav berdurasi ~20 detik**.')
+
 # Define function to predict
 def predict_disease(model, features):
     # Predict
@@ -70,3 +66,9 @@ if uploaded_file is not None:
         st.title('Prediksi: ')
         st.subheader(f'**{clabels_idn[c_pred]}**: {formatted_max}%')
         st.subheader(f'*{clabels[c_pred]}*')
+
+st.markdown('**Konteks Model AI dan Database:**')
+st.caption('Model yang digunakan merupakan Convolutional Neural Network (CNN) yang dilatih menggunakan **TensorFlow 2.11.0**. Model ini dilatih dengan data dari **Respiratory Sound Database** yang dikemas 2 tim peneliti di Portugal dan Yunani atas nama *International Conference on Biomedical Health Informatics* (ICHBI)')
+st.caption('Di sini _library_ **Librosa** digunakan untuk ekstraksi MFCCs dari fail audio. MFCC, atau Mel-Frequency Cepstral Coefficients, merupakan format representasi audio. Dengan proses matematis ini, fitur-fitur penting di frekuensi alami telinga manusia dapat diekstraksi dari fail audio dan dijadikan *input* ke model CNN untuk diprediksi.')
+st.caption('Database yang digunakan dapat dijelajahi dan/atau diunduh di sini: https://bhichallenge.med.auth.gr/')
+st.caption('Jurnal ilmiah menyangkut database dapat dilihat di sini: https://link.springer.com/chapter/10.1007/978-981-10-7419-6_6')
